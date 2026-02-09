3f1ecc72a11926e086c86f3da15a67094997251e
Mahdi Salem <mahhdy@gmail.com>
2026-02-09 01:54:54 -0500

    Add GitHub Actions workflow for deploying Astro to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mahdisalem.com',
+  output: 'static',
   i18n: {
     defaultLocale: 'fa',
     locales: ['fa', 'en'],
